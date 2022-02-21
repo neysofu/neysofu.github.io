26157a712325d5c5e10a0d1c1367b0e5d449f832
Filippo Costa <filippocosta.italy@gmail.com>
2022-02-21 20:16:25 +0100

    Stuff

--- build/images.js
+++ build/images.js
@@ -1,13 +1,1 @@
-const imagemin = require('gulp-imagemin');
-
-const imgPath = 'img/**/*.+(png|jpg|gif|svg)';
-const destPath = '_site/img';
-
-module.exports = gulp => {
-  gulp.task('images', () => {
-    return gulp
-      .src(imgPath)
-      .pipe(imagemin())
-      .pipe(gulp.dest(destPath));
-  });
-};
+const gulp = require('gulp');
