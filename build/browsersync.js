fd5a91252449781a9db6a9d2ac4c0da73a2a1a06
Filippo Costa <filippocosta.italy@gmail.com>
2022-02-21 20:20:19 +0100

    Outdated scripts

--- build/browsersync.js
+++
@@ -1,3 +0,0 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const cp = require('child_process');
--- build/sass.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-
-const scssPath = '_scss/*.scss';
-const destPath = '_site/css';
-
-module.exports = gulp => {
-};
